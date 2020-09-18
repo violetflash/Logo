662fef4be03c4beec9352e4ac3ae1a56f922811d
Alexander Grobovsky <ktotaam@mail.ru>
2020-09-18 12:49:35 +0500

    18.09-work

--- #src/js/files/script.js
+++ #src/js/files/script.js
@@ -1,9 +1,11 @@
 
 let burger = document.querySelector('.icon-menu');
 let burger_menu = document.querySelector('.menu__body');
+let body = document.querySelector('body');
 
 burger.onclick = function () {
     burger.classList.toggle('_active');
     burger_menu.classList.toggle('_active');
+    body.classList.toggle('lock');
 }
 
