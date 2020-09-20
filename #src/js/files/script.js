f36e06869b6c2a406e54c7739e8a25955b7439b0
Alexander Grobovsky <ktotaam@mail.ru>
2020-09-21 01:04:31 +0500

    added page menu

--- #src/js/files/script.js
+++ #src/js/files/script.js
@@ -1,11 +1,47 @@
+let burger_header = document.querySelector('.menu__icon');
 
-let burger = document.querySelector('.icon-menu');
-let burger_menu = document.querySelector('.menu__body');
+let burger_catalog = document.querySelector('.catalog-menu__burger');
+
+let header_burger_menu = document.querySelector('.menu__body');
+
+let catalog_burger_body = document.querySelector('.catalog-menu__body');
+
 let body = document.querySelector('body');
 
-burger.onclick = function () {
-    burger.classList.toggle('_active');
-    burger_menu.classList.toggle('_active');
-    body.classList.toggle('lock');
+let catalog_link = document.querySelector('.catalog-menu__link');
+
+burger_header.onclick = function () {
+  burger_header.classList.toggle('_active');
+  header_burger_menu.classList.toggle('_active');
+  body.classList.toggle('lock');
 }
 
+burger_catalog.onclick = function () {
+  burger_catalog.classList.toggle('_active');
+  catalog_burger_body.classList.toggle('opened');
+}
+
+// catalog_link.onclick = function () {
+//   catalog_link.classList.add('_active')
+//     // document.querySelectorAll('.catalog-menu__item').classList.remove('_active');
+//     // this.classList.toggle('_active')
+// }
+
+
+
+//ЖЕКИНЫ
+
+let menuParents = document.querySelectorAll('.catalog-menu__parent');
+
+let submenuItems = document.querySelectorAll('.submenu__item');
+
+for (let index = 0; index < menuParents.length; index++) {
+    const menuParent = menuParents[index];
+    menuParent.addEventListener("mouseenter", function(e) {
+        menuParent.classList.add('active')
+    });
+
+    menuParent.addEventListener("mouseleave", function(e) {
+      menuParent.classList.remove('active')
+    });
+}
