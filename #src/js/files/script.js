21cc8466467d48df5d5a0952ec01c654404cf999
Alexander Grobovsky <ktotaam@mail.ru>
2020-09-21 12:18:08 +0500

    21.09-work

--- #src/js/files/script.js
+++ #src/js/files/script.js
@@ -1,35 +1,24 @@
-let burger_header = document.querySelector('.menu__icon');
+let burgerHeader = document.querySelector('.menu__icon');
 
-let burger_catalog = document.querySelector('.catalog-menu__burger');
-
-let header_burger_menu = document.querySelector('.menu__body');
-
-let catalog_burger_body = document.querySelector('.catalog-menu__body');
+let burgerHeaderMenu = document.querySelector('.menu__body');
 
 let body = document.querySelector('body');
 
-let catalog_link = document.querySelector('.catalog-menu__link');
-
-burger_header.onclick = function () {
-  burger_header.classList.toggle('_active');
-  header_burger_menu.classList.toggle('_active');
-  body.classList.toggle('lock');
-}
-
-burger_catalog.onclick = function () {
-  burger_catalog.classList.toggle('_active');
-  catalog_burger_body.classList.toggle('opened');
-}
-
-// catalog_link.onclick = function () {
-//   catalog_link.classList.add('_active')
-//     // document.querySelectorAll('.catalog-menu__item').classList.remove('_active');
-//     // this.classList.toggle('_active')
-// }
+burgerHeader.addEventListener("click", function (e) {
+    burgerHeader.classList.toggle('_active');
+    burgerHeaderMenu.classList.toggle('_active');
+    body.classList.toggle('lock');
+});
 
 
+let burgerCatalog = document.querySelector('.catalog-menu__burger');
+let burgerCatalogBody = document.querySelector('.catalog-menu__body');
 
-//ЖЕКИНЫ
+burgerCatalog.addEventListener("click", function (e) {
+    burgerCatalog.classList.toggle('_active');
+    _slideToggle(burgerCatalogBody);
+});
+
 
 let menuParents = document.querySelectorAll('.catalog-menu__parent');
 
@@ -37,11 +26,11 @@ let submenuItems = document.querySelectorAll('.submenu__item');
 
 for (let index = 0; index < menuParents.length; index++) {
     const menuParent = menuParents[index];
-    menuParent.addEventListener("mouseenter", function(e) {
+    menuParent.addEventListener("mouseenter", function (e) {
         menuParent.classList.add('active')
     });
 
-    menuParent.addEventListener("mouseleave", function(e) {
-      menuParent.classList.remove('active')
+    menuParent.addEventListener("mouseleave", function (e) {
+        menuParent.classList.remove('active')
     });
 }
