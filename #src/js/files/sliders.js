068e8dbb929478df8332a633082d553817ecbe46
Alex noteAir <ktotaam@mail.ru>
2020-09-26 19:49:13 +0500

    save

--- #src/js/files/sliders.js
+++ #src/js/files/sliders.js
@@ -29,16 +29,75 @@ if (sliders) {
 
 function sliders_bild_callback(params) { }
 
+
+
+
+try {
+  
+  if (document.querySelector('.mainslider')) {
+
+    let mainslider = new Swiper('.mainslider__body', {
+  
+      // effect: 'fade',
+      // autoplay: {
+      //   delay: 3000,
+      //   disableOnInteraction: false,
+      // },
+      observer: true,
+      observeParents: true,
+      slidesPerView: 1,
+      // spaceBetween: 0,
+      autoHeight: true,
+      speed: 800,
+      loop: true,
+  
+      /*
+      touchRatio: 0,
+      simulateTouch: false,
+      preLoadImages: false,
+      lazy: true,
+      //Dots
+      */
+      pagination: {
+        el: '.mainslider__dots',
+        clickable: true,
+      },
+      // Arrows
+  
+      // navigation: {
+      //   nextEl: '.about__more .about__more--next',
+      //   prevEl: '.about__more .about__more--prev',
+      // },
+      // on: {
+      //   lazyImageReady: function () {
+      //     ibg();
+      //   }, 
+      // }
+    });
+  
+    let mainsliderImages = document.querySelectorAll('.mainslider__image');
+    let mainsliderDots = document.querySelectorAll('.mainslider__dots .swiper-pagination-bullet');
+  
+    for (let index = 0; index < mainsliderImages.length; index++) {
+      const mainsliderImage = mainsliderImages[index + 1].querySelector('img').getAttribute('src');
+      mainsliderDots[index].style.backgroundImage = "url('" + mainsliderImage + "')";
+    }
+  
+  }
+
+}
+catch {}
+
 if (document.querySelector('.products-slider')) {
 
 
   let productsSlider = new Swiper('.products-slider__item', {
 
     // effect: 'fade',
-    autoplay: {
-      delay: 3000,
-      disableOnInteraction: false,
-    },
+    // autoplay: {
+    //   delay: 3000,
+    //   disableOnInteraction: false,
+    // },
     // observer: true,
     // observeParents: true,
     // slidesPerView: 1,
@@ -70,58 +129,6 @@ if (document.querySelector('.products-slider')) {
     //   },
     // }
   });
-
-}
-
-
-if (document.querySelector('.mainslider')) {
-
-  let mainslider = new Swiper('.mainslider__body', {
-
-    // effect: 'fade',
-    // autoplay: {
-    //   delay: 3000,
-    //   disableOnInteraction: false,
-    // },
-    observer: true,
-    observeParents: true,
-    slidesPerView: 1,
-    // spaceBetween: 0,
-    autoHeight: true,
-    speed: 800,
-    loop: true,
-
-    /*
-    touchRatio: 0,
-    simulateTouch: false,
-    preLoadImages: false,
-    lazy: true,
-    //Dots
-    */
-    pagination: {
-      el: '.mainslider__dots',
-      clickable: true,
-    },
-    // Arrows
-
-    // navigation: {
-    //   nextEl: '.about__more .about__more--next',
-    //   prevEl: '.about__more .about__more--prev',
-    // },
-    // on: {
-    //   lazyImageReady: function () {
-    //     ibg();
-    //   }, 
-    // }
-  });
-
-  let mainsliderImages = document.querySelectorAll('.mainslider__image');
-  let mainsliderDots = document.querySelectorAll('.mainslider__dots .swiper-pagination-bullet');
-
-  for (let index = 0; index < mainsliderImages.length; index++) {
-    const mainsliderImage = mainsliderImages[index + 1].querySelector('img').getAttribute('src');
-    mainsliderDots[index].style.backgroundImage = "url('" + mainsliderImage + "')";
-  }
 
 }
 
