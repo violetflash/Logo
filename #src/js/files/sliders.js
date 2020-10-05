ddd6945e469ed38b52593edd7231221e971dbe25
Alex noteAir <ktotaam@mail.ru>
2020-10-05 22:57:11 +0500

    save

--- #src/js/files/sliders.js
+++ #src/js/files/sliders.js
@@ -188,7 +188,7 @@ if (document.querySelector('.page__brands-slider')) {
         slidesPerView: 2,
         spaceBetween: 20
       },
-      // when window width is >= 640px
+      // when window width is >= 768px
       768: {
         slidesPerView: 3,
         spaceBetween: 20
